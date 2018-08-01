0ccfa6f520e7c403742787155cb5a759299b0390
cjh <1418503647@qq.com>
2018-08-02 02:11:39 +0800

    first commit

---
+++ src/views/Home.vue
@@ -0,0 +1,246 @@
+<template>
+    <my-page title="编码解码">
+        <div class="tool-list">
+            <div class="mu-paper list-item mu-paper-round mu-paper-1" 
+                v-for="app in apps">
+                <router-link class="link" :to="app.to" :href="app.href" :target="app.target">
+                    <img :src="app.icon" class="img">
+                    <div class="info">
+                        <h3 class="text">{{ app.name }}</h3>
+                        <div class="desc">{{ app.desc }}</div>
+                    </div>
+                    <i class="icon icon-heart"></i>
+                </router-link>
+            </div>
+        </div>
+    </my-page>
+</template>
+
+<script>
+    export default {
+        data () {
+            return {
+                apps: [
+                    {
+                        name: '编码识别',
+                        desc: '',
+                        icon: '/static/img/coding.svg',
+                        to: '/recognition'
+                    },
+                    {
+                        name: 'Unicode 编码解码',
+                        desc: '',
+                        icon: '/static/img/coding_unicode.svg',
+                        to: 'xxx',
+                        href: 'https://unicode.yunser.com/coding',
+                        target: '_blank'
+                    },
+                    {
+                        name: 'URL 编码解码',
+                        desc: '',
+                        icon: '/static/img/url.svg',
+                        to: 'xxx',
+                        href: 'https://url.yunser.com/',
+                        target: '_blank'
+                    },
+                    {
+                        name: 'Base 64 编码解码',
+                        desc: '',
+                        icon: '/static/img/base64.svg',
+                        to: 'xxx',
+                        href: 'https://base64.yunser.com/',
+                        target: '_blank'
+                    },
+                    {
+                        name: '图片⇆Base64',
+                        desc: '',
+                        icon: '/static/img/base64.svg',
+                        to: 'xxx',
+                        href: 'https://imagetool.yunser.com/base64',
+                        target: '_blank'
+                    },
+                    {
+                        name: '文件哈希值/MD5',
+                        desc: '支持 MD5、SHA1 等散哈希算法',
+                        icon: '/static/img/hash.svg',
+                        to: '//hash/file'
+                    },
+                    {
+                        name: '哈希算法',
+                        desc: '',
+                        icon: '/static/img/hash.svg',
+                        to: '/hash'
+                    },
+                    {
+                        name: 'ASCII 码对照表',
+                        desc: '',
+                        icon: '/static/img/base64.svg',
+                        to: 'xxx',
+                        href: 'https://code.yunser.com/asc2',
+                        target: '_blank'
+                    },
+                    {
+                        name: '字符串转义',
+                        desc: '',
+                        icon: '/static/img/coding.svg',
+                        to: 'xxx',
+                        href: 'https://code.yunser.com/escape',
+                        target: '_blank'
+                    },
+                    {
+                        name: 'HTML 特殊符号',
+                        desc: 'HTML 特殊符号对照表',
+                        icon: '/static/img/html.svg',
+                        to: 'xxx',
+                        href: 'https://code.yunser.com/html/char',
+                        target: '_blank'
+                    },
+                    {
+                        name: '摩斯码编码解码',
+                        desc: '',
+                        icon: '/static/img/morse.svg',
+                        to: 'xxx',
+                        href: 'https://morse.yunser.com/',
+                        target: '_blank'
+                    },
+                    {
+                        name: 'UTF-8 编码解码',
+                        desc: '',
+                        icon: '/static/img/coding.svg',
+                        to: '/utf8'
+                    },
+                    {
+                        name: '编码转换',
+                        desc: '',
+                        icon: '/static/img/coding.svg',
+                        to: '/charset/convert'
+                    },
+                    {
+                        name: '核心价值观编码',
+                        desc: '',
+                        icon: '/static/img/coding.svg',
+                        to: '/core_values'
+                    },
+                    {
+                        name: '文本转 ASCII',
+                        desc: '',
+                        icon: '/static/img/coding.svg',
+                        to: '/text_to_asc2'
+                    },
+                    {
+                        name: '二进制反码补码',
+                        desc: '',
+                        icon: '/static/img/coding.svg',
+                        to: '/binary'
+                    },
+                    {
+                        name: 'Unicode',
+                        desc: '',
+                        icon: '/static/img/coding_unicode.svg',
+                        to: 'xxx',
+                        href: 'https://unicode.yunser.com/',
+                        target: '_blank'
+                    }
+                ]
+            }
+        },
+        computed: {
+        },
+        mounted() {
+        },
+        methods: {
+            init() {
+            },
+            fileChange(e) {
+            },
+            sizeStr: function (size) {
+            }
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+@import '../scss/var';
+
+.tool-list {
+    max-width: 840px;
+    margin: 0 auto;
+    @include clearfix;
+    .list-item {
+        position: relative;
+        float: left;
+        width: 260px;
+        height: 96px;
+        padding: 8px;
+        margin: 2px 16px 16px 2px;
+        background-color: #fff;
+        cursor: pointer;
+        //border: 1px solid #ccc;
+        &:hover {
+            background-color: #f9f9f9;
+            // box-shadow: 0 3px 10px rgba(0,0,0,.156863), 0 3px 10px rgba(0,0,0,.227451);
+            //border-color: #09c;
+            .icon {
+                display: block;
+            }
+        }
+        &.active {
+            border: 1px solid #f00;
+        }
+    }
+    a {
+        display: block;
+        height: 100%;
+        color: #666;
+    }
+    .img {
+        float: left;
+        width: 72px;
+        height: 72px;
+        margin-right: 16px;
+        background-color: #fff;
+        border: 1px solid #e9e9e9;
+        border-radius: 8px;
+    }
+    .info {
+        float: left;
+    }
+    .text {
+        font-size: 18px;
+        color: #000;
+    }
+    .header {
+        overflow: hidden;
+    }
+    .desc {
+        max-width: 150px;
+        margin-top: 8px;
+    }
+    .icon-heart {
+        display: none;
+        position: absolute;
+        top: 8px;
+        right: 8px;
+        &:hover {
+            color: #f00;
+        }
+    }
+    .icon-close {
+        display: none;
+        position: absolute;
+        top: 32px;
+        right: 8px;
+        &:hover {
+            color: #f00;
+        }
+    }
+}
+@media all and (max-width: 400px){
+    .tool-list {
+        .list-item {
+            width: 100%;
+            margin-right: 0;
+        }
+    }
+}
+</style>
---
+++ src/views/Recognition.vue
@@ -0,0 +1,109 @@
+<template>
+    <my-page title="编码识别" :page="page">
+        <textarea class="form-control" v-model="code" rows="6" placeholder="编码内容"></textarea>
+        <div class="btns">
+            <ui-raised-button class="btn" primary label="识别" @click="recognize" />
+        </div>
+        <textarea class="form-control" v-model="result" rows="6" placeholder="转换结果" v-if="result"></textarea>
+    </my-page>
+</template>
+
+<script>
+    export default {
+        data () {
+            return {
+                code: '',
+                result: '',
+                page: {
+                    menu: [
+                        // {
+                        //     type: 'icon',
+                        //     icon: 'help',
+                        //     to: '/help'
+                        // }
+                    ]
+                }
+            }
+        },
+        mounted() {
+            // debug
+            this.code = '\\u8fd9\\u662f\\u6817\\u5b50'
+            // this.code = '0x03,0x9f,0x25'
+            // this.code = '0x03 0x9f  0x25'
+            // this.code = '\\x78\\x78 '
+            // this.code = '%E8%BF%99%E6%98%AF%E4%BB%80%E4%B9%88'
+            // this.code = '&#913;&#914;'
+            // this.code = '友善爱国爱国友善平等友善敬业敬业敬业诚信自由公正敬业爱国友善自由友善敬业诚信自由自由友善平等友善平等爱国富强友善爱国自由友善平等敬业爱国爱国'
+            // this.code = '=E8=BF=99=E6=98=AF=E4=BB=80=E4=B9=88'
+            // this.code = '.---- ..---'
+            // this.recognize()
+        },
+        methods: {
+            recognize() {
+                if (!this.code) {
+                    this.$message({
+                        type: 'danger',
+                        text: '请填写要转换的内容'
+                    })
+                    return
+                }
+                console.log(this.code.match(/^(\\%[0-9A-F]{2})+/))
+                if (this.code.match(/^(\\u[0-9a-f]{4})+$/)) {
+                    this.result = '这是 Unicode 编码'
+                } else if (this.code.match(/^(%[0-9A-F]{2})+/)) {
+                    this.result = '这是 URL 编码'
+                } else if (this.code.match(/^(&#[0-9]{3,5};)+/)) {
+                    this.result = '这是 HTML 码'
+                } else if (this.code.match(/^([富强民主文明和谐自由平等公正法治爱国敬业诚信友善])+/)) {
+                    this.result = '这是核心价值观编码'
+                } else if (this.code.match(/^(=[0-9A-F]{2})+/)) {
+                    this.result = '这是可打印字符引用编码 （quoted-printable 编码）'
+                } else if (this.code.match(/^[ -.]+/)) {
+                    this.result = '这是摩斯码'
+                } else if (this.code.match(/^(0x[0-9a-f]+,)+/)) {
+                    this.result = '这是十六进制数'
+                } else if (this.code.match(/^(0x[0-9a-f]+\s+)+/)) {
+                    this.result = '这是十六进制数'
+                } else if (this.code.match(/^(\\x[0-9a-f]+\s*)+/)) {
+                    this.result = '这是 16 进制的 ASCII 码'
+                } else {
+                    this.result = '没有识别结果。欢迎给我们反馈，帮助我们做得更好。'
+                }
+                // if (this.code.test(/^\\u[0-9a-f]{4}+$/)) {
+                //     console.log('unicode')
+                // }
+                // this.result = this.code.replace(/[^\u0000-\u00FF]/g, function ($0) {
+                //     return escape($0).replace(/(%u)(\w{4})/gi, '&#x$2;')
+                // })
+            }
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+    textarea.form-control {
+        height: auto;
+    }
+    .form-control {
+        display: block;
+        width: 100%;
+        max-width: 400px;
+        height: 33px;
+        padding: 6px 12px;
+        font-size: 14px;
+        line-height: 1.42;
+        color: #55595c;
+        background-color: #fff;
+        background-image: none;
+        border: 1px solid #ccc;
+        border-radius: 2px;
+        outline: none;
+    }
+    .btns {
+        margin-top: 16px;
+        margin-bottom: 16px;
+        .btn {
+            margin-right: 8px;
+        }
+    }
+</style>
